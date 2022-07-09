b83e32aada973e8842e10862857baa1435b15e10
Arturo Sanchez <asanchez@orthocana.com>
2022-07-09 14:42:28 -0500

    # HTTP_SERVER

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# deno-tests
+# Deno Tests
+
+## HTTP Server Demo
+
+File : `httserver.ts`
+
+Live example : https://codesandbox.io/s/deno-http-wvzm4y
--- httserver.ts
+++ httserver.ts
@@ -1,12 +1,17 @@
 import { serve } from "https://deno.land/std@0.147.0/http/server.ts";
 
-const port = 8080;
+class WebServer {
+    constructor(public port: number) {
+        this.port = port;
+        this.handler = this.Handle.bind(this);
+        console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
+        serve(this.handler,{port: this.port});
+    }
 
-const handler = (request : Request): Response => {
-    const body = `Your user-agent is:\n\n${request.headers.get("user-agent") ?? "Unknown"}`;
-    return new Response(body, {status: 200});
+    Handle(request:Request): Response {
+        const body = `Your user-agent is:\n\n${request.headers.get("user-agent") ?? "Unknown"}`;
+        return new Response(body, {status: 200});
+    }
 }
 
-console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
-
-await serve(handler,{port});
+new WebServer(8080);
