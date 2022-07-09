6abd33343593c5886422bc94f8b77b3b0c4ba64e
Arturo Sanchez <asanchez@orthocana.com>
2022-07-09 12:54:55 -0500

    # EXAMPLE/SERVER
    Signed-off-by: Arturo Sanchez <asanchez@orthocana.com>

---
+++ server.ts
@@ -0,0 +1,30 @@
+class Server {
+    constructor(public port: number) {
+      this.port = port;
+      this.server = Deno.listen({ port: this.port });
+
+      console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
+    }
+
+    async ServeHTTP(conn: Deno.Conn) {
+      const httpConn = Deno.serveHttp(conn);
+
+      for await (const requestEvent of httpConn) {
+        const body = `Your user-agent is:\n\n${
+          requestEvent.request.headers.get("user-agent") ?? "Unknown"
+        }`;
+
+        requestEvent.respondWith(
+            new Response(body, {
+              status: 200,
+            }),
+        );
+      }
+    }
+}
+
+let server = new Server(8080);
+
+for await (const conn of server.server) {
+  server.ServeHTTP(conn);
+}
